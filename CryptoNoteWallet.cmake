325f5e5eda5f944766f15d48cc6e3e52c633ad8f
Kyriell <The-K-Man@users.noreply.github.com>
2018-01-15 19:57:45 -0500

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "PlanesWalker")
+set(CN_CURRENCY_DISPLAY_NAME "PlanesWalker")
+set(CN_CURRENCY_TICKER "PlanesWalker")
